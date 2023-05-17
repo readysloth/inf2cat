4224e2921e4e49177568a654a4531f457a22ac12
readysloth <readysloth@protonmail.com>
2023-05-17 15:17:34 +0300

    add(requirements.txt, setup.py):

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(name='inf2cat',
+      version='0.1',
+      packages=['inf2cat'],
+      description='Creates .cat file without even using .inf',
+      build_with_nuitka=True,
+      entry_points={
+          'console_scripts': ['inf2cat = inf2cat:main'],
+      },
+      install_requires=[
+          'cffi==1.15.1',
+          'cryptography==40.0.2',
+          'pycparser==2.21'
+      ])
